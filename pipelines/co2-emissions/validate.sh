e35e28778a4e43873297d10f5da68f3d1bbca0ae
Steven Brus <sbrus@lanl.gov>
2018-09-12 11:12:58 -0600

    Add co2-emissions pipeline

---
+++ pipelines/co2-emissions/validate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# [wf] validate reults and get a table
+set -ex
+
+# [wf] verify that we got actual result values
+scripts/validate_output.py data/global_per_capita_mean.csv
+
+# [wf] generate markdown table
+scripts/get_mdown_table.py data/global_per_capita_mean.csv
+
